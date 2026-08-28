913f9e45d94589a23d9d1b6a6c3b07d131a705f4
bjornnihlen <bjorn.nihlen@gmail.com>
2026-08-28 09:15:51 +0200

    Update to v0.13.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='golf-challenge-0.12.24';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='golf-challenge-0.13.1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
